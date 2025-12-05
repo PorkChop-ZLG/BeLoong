e4a77e3b7c26fd181c92aa8e50777fa7c274e48a
PorkChop_ZLG <2571692644@qq.com>
2025-12-05 16:12:25 +0800

    注册专属龙心

--- kubejs/startup_scripts/items.js
+++ kubejs/startup_scripts/items.js
@@ -1,3 +1,5 @@
+// 贴图存放位置 kubejs\assets\kubejs\textures\item
+// 本地化文件位置 kubejs\assets\kubejs\lang
 StartupEvents.registry("item", (event) => {
   event.create("kubejs:tundra_icon").rarity("rare")
   event.create("kubejs:tundra_full_icon").rarity("rare")
@@ -6,4 +8,10 @@ StartupEvents.registry("item", (event) => {
   event.create("kubejs:worm_food").rarity("epic").tooltip([Text.translatable('item.kubejs.worm_food.tooltip')])
   event.create("kubejs:bloody_spine").rarity("epic").tooltip([Text.translatable('item.kubejs.bloody_spine.tooltip')])
   event.create("kubejs:abeemination").rarity("epic").tooltip([Text.translatable('item.kubejs.abeemination.tooltip')])
+  event.create("kubejs:cave_dragon_heart").rarity("uncommon").tooltip([Text.translatable('item.kubejs.cave_dragon_heart.tooltip')])
+  event.create("kubejs:forest_dragon_heart").rarity("uncommon").tooltip([Text.translatable('item.kubejs.forest_dragon_heart.tooltip')])
+  event.create("kubejs:sea_dragon_heart").rarity("uncommon").tooltip([Text.translatable('item.kubejs.sea_dragon_heart.tooltip')])
+  event.create("kubejs:tundra_dragon_heart").rarity("uncommon").tooltip([Text.translatable('item.kubejs.tundra_dragon_heart.tooltip')])
+  event.create("kubejs:aether_dragon_heart").rarity("uncommon").tooltip([Text.translatable('item.kubejs.aether_dragon_heart.tooltip')])
+  event.create("kubejs:astral_dragon_heart").rarity("uncommon").tooltip([Text.translatable('item.kubejs.astral_dragon_heart.tooltip')])
 })
