39a5929b5ddc6bc96f539ce2ff0b9a472b8dce7d
PorkChop_ZLG <2571692644@qq.com>
2025-12-28 18:25:27 +0800

    为蛙明灯添加配方

--- kubejs/server_scripts/recipe/minecraft.js
+++ kubejs/server_scripts/recipe/minecraft.js
@@ -8,5 +8,26 @@ ServerEvents.recipes(event => {
             'minecraft:white_dye'
         ]
     )
+    // 珠光蛙明灯
+    event.shaped(Item.of('minecraft:pearlescent_froglight', 8), [
+        'AAA',
+        'ABA',
+        'AAA'
+    ],
+        {
+            A: 'minecraft:ochre_froglight',
+            B: 'minecraft:purple_dye'
+        }
+    )
+    // 青翠蛙明灯
+    event.shaped(Item.of('minecraft:verdant_froglight', 8), [
+        'AAA',
+        'ABA',
+        'AAA'
+    ],
+        {
+            A: 'minecraft:ochre_froglight',
+            B: 'minecraft:green_dye'
+        }
+    )
 })
-    
