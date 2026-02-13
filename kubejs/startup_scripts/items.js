5ce86952ab260153545a03728e6906ab3e486491
PorkChop_ZLG <2571692644@qq.com>
2026-02-14 03:53:04 +0800

    始祖龙三件套的贴图，部分语言文件

--- kubejs/startup_scripts/items.js
+++ kubejs/startup_scripts/items.js
@@ -18,7 +18,7 @@ StartupEvents.registry("item", (event) => {
   event.create("kubejs:progenitor_dragon_heart").rarity("epic").tooltip([Text.translatable('item.kubejs.progenitor_dragon_heart.tooltip.1')]).tooltip([Text.translatable('item.kubejs.progenitor_dragon_heart.tooltip.2')])
   event.create("kubejs:progenitor_dragon_power").rarity("epic").tooltip([Text.translatable('item.kubejs.progenitor_dragon_power.tooltip.1')]).tooltip([Text.translatable('item.kubejs.progenitor_dragon_power.tooltip.2')])
   event.create("kubejs:progenitor_dragon_scale").rarity("epic").tooltip([Text.translatable('item.kubejs.progenitor_dragon_scale.tooltip.1')]).tooltip([Text.translatable('item.kubejs.progenitor_dragon_scale.tooltip.2')])
-  event.create("kubejs:progenitor_dragon_skin").rarity("epic").tooltip([Text.translatable('item.kubejs.progenitor_dragon_skin.tooltip.1')]).tooltip([Text.translatable('item.kubejs.progenitor_dragon_skin.tooltip.2')])
+  event.create("kubejs:progenitor_dragon_ball").rarity("epic").tooltip([Text.translatable('item.kubejs.progenitor_dragon_ball.tooltip.1')]).tooltip([Text.translatable('item.kubejs.progenitor_dragon_ball.tooltip.2')])
   // 永恒星光命名空间
   event.create("eternal_starlight:glacite_ingot").tooltip([Text.translatable('item.eternal_starlight.glacite_ingot.tooltip')])
 })
