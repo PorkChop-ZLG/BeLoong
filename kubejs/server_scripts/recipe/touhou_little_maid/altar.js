234107baf114949608062175618a0349cc652ea6
PorkChop_ZLG <2571692644@qq.com>
2026-03-27 19:27:39 +0800

    魂符可以合成

--- kubejs/server_scripts/recipe/touhou_little_maid/altar.js
+++ kubejs/server_scripts/recipe/touhou_little_maid/altar.js
@@ -1,6 +1,6 @@
 // 该脚本用于魔改合成类型 祭坛合成
 ServerEvents.recipes(event => {
-    // 魂符
+    // 空的魂符
     event.custom(
         {
             "type": "touhou_little_maid:altar_recipe_serializers",
@@ -9,29 +9,64 @@ ServerEvents.recipes(event => {
             "group": "altar_recipe",
             "ingredients": [
                 {
-                    "tag": "minecraft:planks"
+                    "item": "ultramarine:xuan_paper"
                 },
                 {
-                    "tag": "minecraft:planks"
+                    "item": "iss_magicfromtheeast:bottle_of_souls"
                 },
                 {
-                    "tag": "minecraft:planks"
+                    "item": "extradelight:honey_cheesecake"
                 },
                 {
-                    "tag": "minecraft:planks"
+                    "item": "extradelight:apple_cheesecake"
                 },
                 {
-                    "item": "minecraft:stone"
+                    "item": "extradelight:pumpkin_cheesecake"
                 },
                 {
-                    "tag": "c:gems/diamond"
+                    "item": "extradelight:glow_berry_cheesecake"
                 }
             ],
             "lang": "jei.touhou_little_maid.altar_craft.item_craft.result",
-            "power": 0.1,
+            "power": 0.5,
             "result": {
                 "count": 1,
-                "id": "touhou_little_maid:bookshelf"
+                "id": "touhou_little_maid:smart_slab_empty"
+            }
+        }
+    )
+    // 满的魂符
+    event.custom(
+        {
+            "type": "touhou_little_maid:altar_recipe_serializers",
+            "category": "misc",
+            "entity": "minecraft:item",
+            "group": "altar_recipe",
+            "ingredients": [
+                {
+                    "item": "ultramarine:xuan_paper"
+                },
+                {
+                    "item": "iss_magicfromtheeast:crystallized_soul"
+                },
+                {
+                    "item": "extradelight:coffe_cake_feast"
+                },
+                {
+                    "item": "extradelight:chocolate_cake_block"
+                },
+                {
+                    "item": "extradelight:lemon_cucumber_cake_item"
+                },
+                {
+                    "item": "extradelight:melon_layer_cake_item"
+                }
+            ],
+            "lang": "jei.touhou_little_maid.altar_craft.item_craft.result",
+            "power": 1.0,
+            "result": {
+                "count": 1,
+                "id": "touhou_little_maid:smart_slab_init"
             }
         }
     )
