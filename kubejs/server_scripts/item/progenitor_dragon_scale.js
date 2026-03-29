6c9c8e7bb60cb70208b3ad9eca8d9009dff84162
PorkChop_ZLG <2571692644@qq.com>
2026-03-29 13:47:28 +0800

    始祖道具不可堆叠，修复创造无法使用的BUG

--- kubejs/server_scripts/item/progenitor_dragon_scale.js
+++ kubejs/server_scripts/item/progenitor_dragon_scale.js
@@ -8,11 +8,8 @@ ItemEvents.rightClicked(event => {
   const server = event.server
   // 如果不是目标物品，忽略
   if (item.id !== "kubejs:progenitor_dragon_scale") return
-  // 判断是否为创造模式
-  if (!player.isCreative()) {
-    // 不是创造模式，消耗物品
-    item.count--
-    // 执行函数，位置 kubejs\data\beloong\function\progenitor_dragon_power.mcfunction
-    server.runCommandSilent(`execute as ${player.getName().getString()} at @s run function beloong:progenitor_dragon_scale`)
-  }
+  // 消耗物品
+  item.count--
+  // 执行函数，位置 kubejs\data\beloong\function\progenitor_dragon_power.mcfunction
+  server.runCommandSilent(`execute as ${player.getName().getString()} at @s run function beloong:progenitor_dragon_scale`)
 })
