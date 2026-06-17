61e07f27f7ae20f07abaf84cf4567acb7a96d833
PorkChop_ZLG <2571692644@qq.com>
2026-06-17 13:27:50 +0800

    紧急修复刷物品BUG

--- kubejs/server_scripts/recipe/minecraft/stonecutting.js
+++ kubejs/server_scripts/recipe/minecraft/stonecutting.js
@@ -2,7 +2,7 @@
 
 ServerEvents.recipes(event => {
     // 下界合金锭 切成 下界合金碎片
-    event.stonecutting('4x minecraft:netherite_scrap', 'minecraft:netherite_ingot')
+    event.stonecutting('2x minecraft:netherite_scrap', 'minecraft:netherite_ingot')
     // 始祖龙心 切成 专属龙心
     event.stonecutting('kubejs:cave_dragon_heart', 'kubejs:progenitor_dragon_heart')
     event.stonecutting('kubejs:forest_dragon_heart', 'kubejs:progenitor_dragon_heart')
